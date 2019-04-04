d16acd82d02b3643b615a3050649318b7cbf95d2
Michael Checo <michaeljcheco@gmail.com>
2019-04-04 22:26:29 +0000

    Addition of orginal image

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+var api_url =  "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $("#search-button").click(function(){
-  
-  
-  
+    $.ajax({
+        url: api_url,
+        method: "GET",
+        success: function(response) {
+        $("body").append('<img src="' + response.data[0].images.original.url + '"/>');
+        }
+    });
 });
-
